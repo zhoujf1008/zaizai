1c468b28bbe1a35d543f82ea34fb8a5a2eacb3f3
zhoujf1008 <2437150221@qq.com>
2020-01-09 13:44:05 +0800

    ceshi

--- src/views/manager/address/Edit.vue
+++ src/views/manager/address/Edit.vue
@@ -1,7 +1,7 @@
 <template>
   <briup-fulllayout title="新增地址">
     <div>
-      {{form}}
+      
       <van-cell-group>
         <van-field v-model="form.telephone" placeholder="手机号" />
       </van-cell-group>
